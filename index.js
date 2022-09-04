c5462c0a9bacee1efee0b7987ddaf185df8d6276
Farrukh Ameen <engr.farrukhameen@gmail.com>
2022-09-04 11:55:07 +0500

    prettify

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 /* DO NOT MODIFY */
 const rootElement = document.querySelector(".foods");
 const foodData = [
-  {
-    id: 1,
-    image: "🌮",
-    name: "taco"
-  },
-  {
-    id: 2,
-    image: "🍔",
-    name: "burger"
-  },
-  {
-    id: 3,
-    image: "🍎",
-    name: "apple"
-  },
-  {
-    id: 4,
-    image: "🥞",
-    name: "pancakes"
-  }
+    {
+        id: 1,
+        image: "🌮",
+        name: "taco"
+    },
+    {
+        id: 2,
+        image: "🍔",
+        name: "burger"
+    },
+    {
+        id: 3,
+        image: "🍎",
+        name: "apple"
+    },
+    {
+        id: 4,
+        image: "🥞",
+        name: "pancakes"
+    }
 ];
 
 /** Write code below**/
 
 class Food {
-  constructor(list, root) {
-    const food = document.querySelector(root);
-    list.forEach(
-      (item) =>
-        (food.innerHTML =
-          food.innerHTML +
-          `<div class='food'>
+    constructor(list, root) {
+        const food = document.querySelector(root);
+        list.forEach(
+            (item) =>
+                (food.innerHTML =
+                    food.innerHTML +
+                    `<div class='food'>
       ${item.image}  ${item.name}
     </div>`)
-    );
-    const nodes = document.querySelectorAll(".food");
-    nodes.forEach((n) => n.addEventListener("click", this.handleClick, false));
-  }
+        );
+        const nodes = document.querySelectorAll(".food");
+        nodes.forEach((n) => n.addEventListener("click", this.handleClick, false));
+    }
 
-  handleClick = (e) => {
-    e.target.remove();
-  };
+    handleClick = (e) => {
+        e.target.remove();
+    };
 }
 
 new Food(foodData, ".foods");
